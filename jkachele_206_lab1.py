7475b2414891b1202991ec7ab05e31ab20f1a657
jkachele <justin@kachele.com>
2026-08-27 15:56:24 -0400

    Added Lab 1

---
+++ jkachele_206_lab1.py
@@ -0,0 +1,38 @@
+'''
+Rename this file: netID_2xx_lab1.py 
+(netID is your GMU email before the @, 2xx is your lab section)
+-------------------------------------------------------------------------------
+Name: Justin Kachele
+Assignment: Lab 1
+Due Date: August 27, 2026
+-------------------------------------------------------------------------------
+'''
+
+def sum_three(a, b, c):
+    result = a + b + c
+    return result
+
+#***remove the # from each of the lines below***
+def square_it(num):
+    result = num ** 2
+    return (result)
+
+'''
+Type your answer to the question: "Why did the computer scientist have their drivers license revoked?"
+"Because they kept crashing!"
+
+
+Type the name and major of the two students you worked with today during lab:
+Name: Elizabeth Ford        Major: Bio Engineering
+Name: Belen Tellez          Major: Mechanical Engineering
+'''
+
+
+'''
+Submit this file to Lab 1 in Gradescope once you're done.
+Congrats, you have completed your first lab for CS112!
+
+*Remember: We drop up to 2 labs in total for the whole semester, 
+so you can be absent for a total of 2 labs (2 weeks) and not lose 
+any points. However, we highly recommend you don't miss any labs.
+'''
